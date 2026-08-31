3377d4d4660ceae242cbf77bc30c3c5e42fc9c88
M4RC086 <marcomeulic@gmail.com>
2026-09-01 00:14:30 +0200

    main idea w/o db

--- index.py
+++ index.py
@@ -1,12 +1,49 @@
 #!/bin/python
+from flask import Flask, render_template, jsonify
+from random import choice
 
-from flask import Flask, render_template
+BEGIN_RANDOM_MIN = 80
+score = 10
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("game.html")
+    return render_template("game.html", score=score)
 
 
-app.run(host="0.0.0.0", port=5000)
+
+@app.route('/change-score', methods=['POST'])
+def changeScore(): 
+    global score
+
+    the_ultimate_choice = choice([True, True, True, True, False])
+
+    if (the_ultimate_choice or score <= BEGIN_RANDOM_MIN):
+        score *= 2
+    else:
+        score = 20
+
+
+    str_score = "$"+str(score)
+    return jsonify({"new_value": str_score})# It does some js stuff I'm afraid of
+
+@app.route('/stay', methods=['POST'])
+def stay():
+    global score
+    score = 20
+    return "score updated :)"
+
+
+def get_db_score():
+    # Get the score from the database
+    pass
+
+def set_db_score():
+    # Change the score of the database
+    pass
+
+
+app.run(debug=True, host="0.0.0.0", port=5000)
